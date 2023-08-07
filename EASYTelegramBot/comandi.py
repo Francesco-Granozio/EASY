4f9c345ea6189961b78ddbc50a408067b503426d
Francescoo45 <granoziofrancesco.1@outlook.it>
2023-08-08 00:05:45 +0200

    first commit

---
+++ EASYTelegramBot/comandi.py
@@ -0,0 +1,69 @@
+# Costanti letterali per opzioni
+OPZIONI_NICKNAME = 'Nickname ✏️'
+OPZIONI_QUIZ = 'Quiz 📝'
+
+# Costanti letterali per argomenti del quiz
+QUIZ_CONCETTI_BASE = 'Concetti base del linguaggio C 📚'
+QUIZ_ESPRESSIONI_CICLI = 'Espressioni condizionali e cicli 🔁'
+QUIZ_FUNZIONI = 'Funzioni 🧩'
+QUIZ_ARRAY_PUNTATORI = 'Array e puntatori 🎯'
+QUIZ_STRINGHE = 'Stringhe 📜'
+QUIZ_STRUTTURE_DATI = 'Strutture dati 🗄️'
+QUIZ_GESTIONE_MEMORIA = 'Gestione della memoria 💾'
+QUIZ_FILES = 'Files 🗃️'
+QUIZ_ISTRUZIONI_PREPROCESSORE = 'Istruzioni di pre-processore ⚙️'
+
+TORNA_INDIETRO = "Torna indietro ↩️"
+
+# Costanti letterali per i powerups
+POWERUP_SUPERVELOCITA = 'Super Velocità 🏃‍♂️'
+POWERUP_STREAK = "Streak 🔥"
+POWERUP_REGALO = "Regalo 🎁"
+POWERUP_DOPPIO_RISCHIO = "Doppio Rischio 🎲"
+POWERUP_2X = "2x 2️⃣"
+POWERUP_50_50 = "50/50 🔘"
+POWERUP_GOMMA = "Gomma 🧼"
+POWERUP_SECONDA_CHANCE = "Seconda Chance 🔄"
+POWERUP_FREEZE = "Freeze ❄️"
+POWERUP_IMMUNITA = "Immunità 🛡️"
+POWERUP_GIOCO_DI_POTERE = "Gioco di Potere 💥"
+POWERUP_RADDOPPIA_TEMPO = "Raddoppia Tempo ⏱️"
+POWERUP_RIDUCI_TEMPO_RISPOSTA = "Riduci Tempo Risposta ⏲️"
+POWERUP_AUMENTA_TEMPO_RISPOSTA = "Aumenta Tempo Risposta ⏳"
+
+powerups = {
+    POWERUP_SUPERVELOCITA: 'Il giocatore guadagna 2x punti per 30 secondi se risponde ad una velocità maggiore',
+    POWERUP_STREAK: "Il giocatore aumenta la sua streak di 3 punti",
+    POWERUP_REGALO: "Il giocatore riceve un numero di punti casuale tra 1 e 10",
+    POWERUP_DOPPIO_RISCHIO: "Il giocatore guadagna 2x punti se la risposta è corretta, altrimenti perde 2x punti",
+    POWERUP_2X: "Il giocatore guadagna 2x punti",
+    POWERUP_50_50: "Vengono eliminate 2 risposte sbagliate",
+    POWERUP_GOMMA: "Viene eliminata 1 risposta sbagliata",
+    POWERUP_SECONDA_CHANCE: "Il giocatore ha una seconda possibilità di rispondere",
+    POWERUP_FREEZE: "Il tempo si ferma finchè non si risponde alla domanda",
+    POWERUP_IMMUNITA: "Il giocatore non perde punti se la risposta è sbagliata",
+    POWERUP_GIOCO_DI_POTERE: "Il giocatore guadagna 2x punti se la risposta è corretta, mentre gli altri giocatori "
+                             "perdono 2x punti in caso di risposta sbagliata",
+    POWERUP_RADDOPPIA_TEMPO: "Il tempo per rispondere alla domanda viene raddoppiato",
+    POWERUP_RIDUCI_TEMPO_RISPOSTA: "Gli altri giocatori hanno meno tempo per rispondere alla domanda",
+    POWERUP_AUMENTA_TEMPO_RISPOSTA: "Gli altri giocatori hanno più tempo per rispondere alla domanda",
+}
+
+opzioni = {
+    OPZIONI_NICKNAME: 'Cambia il tuo nickname',
+    OPZIONI_QUIZ: 'Inizia un quiz su un argomento a tua scelta',
+}
+
+quiz_topics = {
+    QUIZ_CONCETTI_BASE: 'Tipi di dati, input e output, operatori e commenti',
+    QUIZ_ESPRESSIONI_CICLI: 'Espressioni condizionali if, else, else if, switch, cicli while e for',
+    QUIZ_FUNZIONI: 'Descrizione degli argomenti sulle funzioni',
+    QUIZ_ARRAY_PUNTATORI: 'Descrizione degli argomenti sugli array e i puntatori',
+    QUIZ_STRINGHE: 'Descrizione degli argomenti sulle stringhe',
+    QUIZ_STRUTTURE_DATI: 'Descrizione degli argomenti sulle strutture dati',
+    QUIZ_GESTIONE_MEMORIA: 'Descrizione degli argomenti sulla gestione della memoria',
+    QUIZ_FILES: 'Descrizione degli argomenti sui file',
+    QUIZ_ISTRUZIONI_PREPROCESSORE: 'Descrizione degli argomenti sulle istruzioni di pre-processore',
+    TORNA_INDIETRO: 'Torna indietro'
+}
+
